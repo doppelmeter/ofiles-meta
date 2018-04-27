dd02ce547cc319a72d0ab52f7830c98a568fdb71
Marius Hürzeler <huerzeler.marius@gmail.com>
2018-04-27 21:38:52 +0200

    adding setup.py and build.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ofiles_meta',
+    version='0.0.0',
+    packages=['ofiles_meta', 'ofiles_meta.ocad', 'ofiles_meta.mapper'],
+    url='https://github.com/huema-ch/ofiles-meta',
+    license='MIT',
+    author='Marius Hürzeler',
+    author_email='huerzeler.marius@gmail.com',
+    description='provide functions to analyze files related to orienteering'
+)
